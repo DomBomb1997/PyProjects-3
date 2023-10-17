0dee7dee51e860d9ff716a81850fc89408ae00e2
DomBomb1997 <53356399+DomBomb1997@users.noreply.github.com>
2023-10-16 18:44:28 -0700

    Add files via upload

--- src/mainapp/products/admin.py
+++ src/mainapp/products/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
+from .models import Products
 
-from .models import Product
-
-
-admin.site.register(Product)
+admin.site.register(Products)
+# Register your models here.
